2166a6c497f8773a08a3dbc7800b865b0eeabb2a
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-17 13:16:36 +0530

    Add Delete Comment Feature

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,7 +3,7 @@ const Post = require('../models/post');
 
 module.exports.create = function(req,res){
     Post.findById(req.body.post,function(err,post){
-        if(err){
+        if(err||!post){
             console.log('The post for which comment is intended could not be found ',err);
             return;
         }
@@ -24,5 +24,20 @@ module.exports.create = function(req,res){
                 }
             })
         }
+    });
+}
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+       if(comment.user==req.user.id){
+           let postId = comment.post;
+           comment.remove();
+           Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
+              return res.redirect('back');
+           });
+       }
+       else{
+        return res.redirect('back');
+       }
     });
 }
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const passport = require('passport');
 const comments_Controller = require('../controllers/comments_controller');
 
 router.post('/create',passport.checkAuthentication,comments_Controller.create);
+router.get('/destroy/:id',passport.checkAuthentication,comments_Controller.destroy);
 
 module.exports = router;
