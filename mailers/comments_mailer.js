d8d3778365d22a2fa1ca36b492d488dcf0fe6b4d
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-27 20:38:14 +0530

    use template to send mails

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,12 +1,13 @@
 const nodeMailer = require('../config/nodemailer');
 
 module.exports.newComment = function(comment){
-    console.log('Inside new comment mailer');
+    let htmlString = nodeMailer.renderTemplate({comment:comment},'/comments/newComment.ejs');
+
     nodeMailer.transporter.sendMail({
        from:'innerlight436@gmail.com',
        to:comment.user.email,
        subject:"New Comment Published",
-       html:'<h1>It is Working</h1>'
+       html:htmlString
     },function(err,info){
         if(err){
             console.log('Error in sending email',err);
