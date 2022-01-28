3cbe9fb321d3e762841f1df31b32c0920f266a50
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-28 19:04:44 +0530

    set up like schema

---
+++ models/like.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    //this defines the object id of the liked object. refPath is used for dynamic referencing
+    likeable:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath:'onModel'
+    },
+    // this field is used for defining the type of the liked object since this is a dynamic reference
+    onModel:{
+        type:String,
+        required:true,
+        enum:['Post','Comment']
+    }
+},{
+    timestamps:true
+});
+
+const Like = mongoose.model('Like',likeSchema);
+
+module.exports = Like;
+
